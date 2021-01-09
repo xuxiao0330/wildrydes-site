36e1f5b88ea269680efd227146f813e7dbe85f69
Xiao Xu <xuxiao0330@hotmail.com>
2021-01-10 01:18:33 +1100

    sssssssave new

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_nuVHUGkrs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'arn:aws:cognito-idp:ap-southeast-2:810726176861:userpool/ap-southeast-2_nuVHUGkrs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4n8hitt843513ask8llgvtddj9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
