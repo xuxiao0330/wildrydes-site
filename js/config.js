4c295975c6327308cb3e2ac91bb04b02c1182063
Xiao Xu <xuxiao0330@hotmail.com>
2021-01-10 01:56:51 +1100

    iiiiiitgusdhfsfdd

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://6squ0hm6f9.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
